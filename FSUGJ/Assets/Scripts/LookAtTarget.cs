a040a6d877abb676e1f43390a05d4991abb64a0a
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-11-12 13:36:51 -0500

    Initial Scene
    With example assets/LookAtTarget

---
+++ FSUGJ/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position);
+    }
+}
