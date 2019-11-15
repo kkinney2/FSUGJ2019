38b86e4030f02cc785dad52236e9e0513c865e4f
Unknown <kkinney2@student.fitchburgstate.edu>
2019-11-14 23:59:11 -0500

    Random Numbers
    with '1' being spawned first to ensure spawning

--- FSUGJ/Assets/Scripts/CharacterSpawner.cs
+++ FSUGJ/Assets/Scripts/CharacterSpawner.cs
@@ -6,6 +6,7 @@ public class CharacterSpawner : MonoBehaviour
 {
     public GameObject prefab_Character;
     public GameObject[] characters;
+    public GameObject[] numbers;
     public int numOfCharacters = 5;
     GameObject[] taskLocations;
 
@@ -14,10 +15,29 @@ public class CharacterSpawner : MonoBehaviour
     {
         taskLocations = GameObject.FindGameObjectsWithTag("Task_Location");
 
-        for (int i = 0; i < numOfCharacters; i++)
+        GameObject a_Character = Instantiate(characters[Random.Range(0, characters.Length - 1)]);
+        a_Character.transform.SetParent(this.transform);
+
+        // Spawn the '1'
+        GameObject a_Number = Instantiate(numbers[0]);
+
+        a_Number.transform.position = a_Character.GetComponent<Character>().body_Num.transform.position;
+        a_Number.transform.SetParent(a_Character.GetComponent<Character>().body_Num.transform);
+
+        a_Character.transform.position = taskLocations[Random.Range(0, taskLocations.Length - 1)].transform.position;
+
+        for (int i = 0; i < numOfCharacters-1; i++)
         {
             //GameObject a_Character = Instantiate(prefab_Character);
-            GameObject a_Character = Instantiate(characters[Random.Range(0, characters.Length-1)]);
+            a_Character = Instantiate(characters[Random.Range(0, characters.Length-1)]);
+            a_Character.transform.SetParent(this.transform);
+
+            // Spawn any other number besides the '1'
+            a_Number = Instantiate(numbers[Random.Range(1, numbers.Length-1)]);
+
+            a_Number.transform.position = a_Character.GetComponent<Character>().body_Num.transform.position;
+            a_Number.transform.SetParent(a_Character.GetComponent<Character>().body_Num.transform);
+
             a_Character.transform.position = taskLocations[Random.Range(0, taskLocations.Length - 1)].transform.position;
         }
     }
